4897b9771e3f1e284717d174fcae2d8af36868d2
Ridlo Alimudin <ridlo.alimudin@gmail.com>
2025-02-07 12:34:53 +0700

    fix: update CORS middleware to restrict Access-Control-Allow-Origin and enable credentials

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -106,9 +106,10 @@ func initializeGinEngine(db *sql.DB, rdb *rdbDependency.Client, productUsecase *
 	r := gin.Default()
 
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://hartakarun.click")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusOK)
